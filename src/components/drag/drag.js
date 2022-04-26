20e4131b37930c4d322d404bd897f43537a43201
Geovane Araújo <geovane-araujo@outlook.com>
2022-04-26 00:48:40 -0300

    deu boa o drag

--- src/components/drag/drag.js
+++ src/components/drag/drag.js
@@ -4,41 +4,40 @@ export default {
   name: 'DragComponent',
   setup () {
     let drg = ref(null)
-    let roll = []
+    let temp = ''
     let coordenadas = ref(Object)
     var countelements = 0
     onMounted(() => {
     })
     const onClick = () => {
-      drg.value.appendChild(onCreateElement())
+      var element = onCreateElement()
+      drg.value.appendChild(element)
+      element.addEventListener('mousedown', onStart)
     }
     const onCreateElement = () => {
       var element = document.createElement('div')
       element.id = 'element_id_' + (countelements += 1)
-      element.style.cssText = 'position:absolute;border:solid 1px;height:50px;width:200px';
-      var coord = {
-        x: 0,
-        y: 0
-      }
-      coordenadas.value[element.id] = coord
-      element.addEventListener('mousedown', onStart)
+      element.style.cssText = 'position:absolute;border:solid 1px;height:50px;width:200px;top:10px;';
+      coordenadas.value[element.id] = { x: 0, y: 0 }
+      
       return element
     }
     const onStart = (e) => {
-      coordenadas.value[e.srcElement.id].x = e.pageX - document.getElementById(e.srcElement.id).offsetLeft
-      coordenadas.value[e.srcElement.id].y = e.pageY - document.getElementById(e.srcElement.id).offsetTop
       var element = document.getElementById(e.srcElement.id)
-
-      element.addEventListener('mousemove', onMove(e,e.srcElement.id))
-      element.addEventListener('mouseup', onEnd(e,e.srcElement.id))
+      coordenadas.value[e.srcElement.id].x = e.pageX - element.offsetLeft
+      coordenadas.value[e.srcElement.id].y = e.pageY - element.offsetTop
+      temp = e.srcElement.id
+      addEventListener('mousemove', onMove)
+      addEventListener('mouseup', onEnd)
     }
-    const onMove = (e, id) => {
-      document.getElementById(id).style.left = (e.pageX - coordenadas.value[e.srcElement.id].x) + 'px'
-      document.getElementById(id).style.top = (e.pageY - coordenadas.value[e.srcElement.id].y) + 'px'
+    const onMove = (e) => {
+      var x = (e.pageX - coordenadas.value[temp].x)
+      var y = (e.pageY - coordenadas.value[temp].y)
+      document.getElementById(temp).style.cssText = 'position:absolute;border:solid 1px;height:50px;width:200px;' + 'top:' + y + 'px;left:' + x + 'px;';
     }
-    const onEnd = (e, id) => {
-      document.getElementById(id).removeEventListener("mousemove", onMove);
-      document.getElementById(id).removeEventListener("mouseup", onEnd);
+    const onEnd = () => {
+      removeEventListener("mousemove", onMove);
+      removeEventListener("mouseup", onEnd);
     }
     return { onClick, drg }
   }
